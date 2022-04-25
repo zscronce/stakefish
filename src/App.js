e7aa0b01fe09b135f68cf9322ae50bc614341dea
Zach Scronce <zscronce@gmail.com>
2022-04-25 17:02:58 -0400

    render loader or coingecko data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch('https://api.coingecko.com/api/v3/exchanges/list')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
+  renderData() {
+    return <p>{JSON.stringify(this.state.data)}</p>;
+  }
+
+  renderLoader() {
+    return (
+      <div className="progress">
+        <div className="indeterminate"/>
+      </div>
+    );
+  }
+
+  render() {
+    return this.state.data ? this.renderData() : this.renderLoader();
+  }
 }
 
 export default App;
