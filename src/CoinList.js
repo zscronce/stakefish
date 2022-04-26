0af88835bbacd61c075aa9b98538e4067c877065
Zach Scronce <zscronce@gmail.com>
2022-04-26 15:24:45 -0400

    add some styling and more information to exchange list

--- src/CoinList.js
+++ src/CoinList.js
@@ -1,19 +1,32 @@
 export function CoinListItem(props) {
+    const { coin } = props;
     return (
-        <li>
-            <a href={'#' + props.id}>
-                {props.name}
+        <li className="collection-item avatar">
+            <a href={'#' + coin.id}>
+                <img className="circle" alt="" src={coin.image}/>
             </a>
+            <span className="title">
+                {coin.trust_score_rank + ' - '}
+                <a href={'#' + coin.id}>
+                    <strong>
+                        {coin.name}
+                    </strong>
+                </a>
+            </span>
+            <p>
+                <a href={coin.url}>{coin.url}</a> ({coin.country})
+            </p>
         </li>
     );
 }
 
 export function CoinList(props) {
     return <>
-        <h4>Coingecko Exchanges</h4>
-        <ul>
+        <h4 className="header">Coingecko Exchanges</h4>
+        <h6>Listed by Trust Rank</h6>
+        <ul className="collection">
             { props.coins.map(coin =>
-                <CoinListItem key={coin.id} id={coin.id} name={coin.name}/>
+                <CoinListItem key={coin.id} coin={coin}/>
             )}
         </ul>
     </>;
